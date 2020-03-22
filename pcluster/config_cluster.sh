78dec291a81248602fa58006ed65ba001d9af41a
cosunae <carlos.osuna@meteoswiss.ch>
2020-03-22 14:04:08 +0100

    small syntax fixes

--- pcluster/config_cluster.sh
+++ pcluster/config_cluster.sh
@@ -2,7 +2,7 @@
 
 ip=$1
 
-scp -i ~/.ssh/calico.pem ~/.ssh/id_github ${ip}/.ssh/
+scp -i ~/.ssh/calico.pem ~/.ssh/id_github ${ip}:~/.ssh/
 
 ssh -i ~/.ssh/calico.pem ${ip} mkdir /scratch/build
 
@@ -10,4 +10,4 @@ scp -i ~/.ssh/calico.pem patch.mpif-sizeof.h ${ip}:/scratch/build
 scp -i ~/.ssh/calico.pem postinstall.sh ${ip}:/scratch/build
 scp -i ~/.ssh/calico.pem patch.mpif-sizeof.h ${ip}:/scratch/build
 
-ssh -i ~/.ssh/calico.pem patch mpif-sizeof.h.orig.test  > patch.mpif-sizeof.h 
+ssh -i ~/.ssh/calico.pem ${ip} patch /opt/amazon/openmpi/include/mpif-sizeof.h /scratch/build/patch.mpif-sizeof.h 
